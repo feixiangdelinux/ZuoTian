0130c0b0f33800da0e1867dde8bdd1b8ee64a05a
linux <linux2005@163.com>
2021-01-06 17:11:32 +0800

    222

---
+++ zzz.py
@@ -0,0 +1,80 @@
+import time
+
+from pynput.keyboard import Controller, Key
+
+from pynput.keyboard import Controller
+
+from util.MouseUtil import MouseUtil
+from util.ZuoTianUtil import click_tool_bar, right_goods
+
+keyboard = Controller()
+list1 = ['a979899', 'b979899', 'c979899', 'd979899', 'e979899']
+screen = [1920, 1080]
+# 显示桌面
+MouseUtil().right_click(screen[0], screen[-1])
+keyboard.press('s')
+keyboard.release('s')
+
+for user in list1:
+    print(list1.index(user))
+    # 打开大话西游
+    MouseUtil().move_to(36, 44)
+    MouseUtil().click_left_two()
+    time.sleep(6)
+    # 堆叠窗口
+    MouseUtil().right_click(1440 - 100, screen[-1])
+    keyboard.press('e')
+    keyboard.release('e')
+
+    MouseUtil().left_click(680, 170)
+    MouseUtil().left_click(260, 155)
+    MouseUtil().left_click(600, 270)
+    time.sleep(1)
+    for i in range(10):
+        keyboard.press(Key.backspace)
+        keyboard.release(Key.backspace)
+    time.sleep(1)
+    for every_char in user:
+        keyboard.press(every_char)
+        keyboard.release(every_char)
+    time.sleep(1)
+    MouseUtil().left_click(600, 320)
+    time.sleep(1)
+    for i in range(10):
+        keyboard.press(Key.backspace)
+        keyboard.release(Key.backspace)
+    time.sleep(1)
+    for every_char in user:
+        keyboard.press(every_char)
+        keyboard.release(every_char)
+    time.sleep(1)
+    MouseUtil().left_click(350, 400)
+    # 选择第一个人物
+    MouseUtil().left_click(200, 290)
+    MouseUtil().left_click(150, 540)
+    MouseUtil().left_click(400, 510)
+    for i in range(3):
+        MouseUtil().left_click(110, 595)
+    click_tool_bar(2)
+    MouseUtil().left_click(410, 300)
+    click_tool_bar(2)
+    MouseUtil().left_click(500, 200)
+    click_tool_bar(2)
+    for i in range(4):
+        MouseUtil().left_click(400, 265)
+        MouseUtil().left_click(315, 425)
+    click_tool_bar(2)
+    click_tool_bar(10)
+    MouseUtil().left_click(350, 425)
+    MouseUtil().left_click(350, 425)
+    click_tool_bar(10)
+    for i in range(5):
+        MouseUtil().left_click(315 + (i * 46), 50)
+        if i == 0:
+            click_tool_bar(1)
+            right_goods(4, 4)
+        right_goods(4, 5)
+        right_goods(4, 6)
+    MouseUtil().left_click(315, 50)
+    MouseUtil().left_click((list1.index(user) + 2) * 160 - 29, screen[-1] - 20)
+    MouseUtil().left_click((list1.index(user) + 2) * 160 - 29, screen[-1] - 20)
