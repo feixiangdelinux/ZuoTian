28dd4b86b83f20a2214bc6b0367ae6cb4c597841
linux <linux2005@163.com>
2021-01-07 16:16:15 +0800

    22256354

--- opendh.py
+++ opendh.py
@@ -30,15 +30,8 @@ list1 = ['a979899', 'b979899', 'c979899', 'd979899', 'e979899']
 #     MouseUtil().left_click((i + 2) * 160 - 29, 880)
 # for i in range(5):
 
-
-
 for i in list1:
     print(list1.index(i))
-
-
-
-
-
 
 # MouseUtil().move_to(51, 880)
 # MouseUtil().move_to(211, 880)
@@ -50,10 +43,6 @@ for i in list1:
 # MouseUtil().move_to(131, 900)
 
 # MouseUtil().move_to((160 * 1) - 29, 880)
-
-
-
-
 
 # # 显示桌面
 # MouseUtil().right_click(1440, 1080)
@@ -67,7 +56,6 @@ for i in list1:
 #     keyboard.press('s')
 #     keyboard.release('s')
 # MouseUtil().left_click((0 + 2) * 160 - 29, 880)
-
 
 # MouseUtil().move_to(680, 170)
 # MouseUtil().move_to(260, 155)
